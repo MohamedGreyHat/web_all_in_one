09613f7188303958026d742dfc67cfd7f99716b9
MedGreyHat <mohamed_ouldmiloud@outlook.com>
2019-10-04 19:50:23 +0100

    [develop]: [+] ajout traduction registration

--- django_project/settings.py
+++ django_project/settings.py
@@ -106,7 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'UTC'
 
