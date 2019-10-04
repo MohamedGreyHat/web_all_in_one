38d3deda13bf6fcea6340ce3fb393126ee539413
MedGreyHat <mohamed_ouldmiloud@outlook.com>
2019-10-04 19:41:07 +0100

    [develop]: [+] Registration after crispy_forms

--- blog/views.py
+++ blog/views.py
@@ -2,21 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 
-# POSTS = [
-#     {
-#         'author':'Bot 1',
-#         'title':'Post 1',
-#         'date_posted':'Mai 1st 1994',
-#         'content': 'First post content',
-#     },
-#     {
-#         'author':'Bot 2',
-#         'title':'Post 2',
-#         'date_posted':'December 31 1999',
-#         'content': 'Second post content',
-#     },
-# ]
-
 def home(request):
     context = {
         'posts': Post.objects.all(),
